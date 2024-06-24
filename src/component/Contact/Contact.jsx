cdd9a1d28bfe46c4018d160e0cd6e99d0bc6d37c
Jiranon-K <jiranon46@gmail.com>
2024-06-24 22:06:25 +0700

    Email-send update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 import styles from './Contact.module.css'
 import { FaPhone, FaEnvelope } from "react-icons/fa6";
 
-function Contact() {
+function Contact() { 
+
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_m1u0p99', 'template_o0oyfn3', e.target, {
+                publicKey: 'Z12sie7-3-23t2XvC',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
+
+        
         <div id='contact' className={styles.contact_wrapper}>
             <div className={styles.contact_con}>
                 <div className={styles.contact_info}>
@@ -18,28 +40,29 @@ function Contact() {
                     <p><FaPhone /> (+66) 97 918 7993</p>
                     <p><FaEnvelope /> jiranon46@gmail.com</p>
                 </div>
-                <div className={styles.contact_form}>
-                    <form action="">
+                <div className={styles.contact_form} >
+
+                    <form className='form' onSubmit={sendEmail} >
                         <div className={styles.input_group}>
                             <div>
                                 <label htmlFor="first name">First Name</label>
-                                <input type="text" placeholder='eg. John' />
+                                <input type="text" name='firstname' placeholder='eg. John' />
                             </div>
                             <div>
                                 <label htmlFor="last name">Last Name</label>
-                                <input type="text" placeholder='eg. Doe' />
+                                <input type="text" name='lastname' placeholder='eg. Doe' />
                             </div>
                         </div>
                         <label htmlFor="email address">Email address</label>
-                        <input type="email" placeholder='eg. johndoe@gmail.com' />
+                        <input type="email" name='user_email' placeholder='eg. johndoe@gmail.com' />
                         <label htmlFor="email address">Your message</label>
-                        <textarea name="" id="" cols="30" rows="10" placeholder='Type here'></textarea>
-                        <button type='submit'>Submit</button>
+                        <textarea name="message" id="" cols="30" rows="10" placeholder='Type here'></textarea>
+                        <button type='submit' value="Send" >Submit</button>
                     </form>
                 </div>
             </div>
         </div>
-    )
+   )
 }
 
 export default Contact
