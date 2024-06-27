2cad2393418b364765454e5c61d2e81cd8630840
Jiranon-K <jiranon46@gmail.com>
2024-06-27 16:07:32 +0700

    #

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -9,8 +9,8 @@ function Portfolio() {
             <div className={styles.port_list}>
                 <div className={styles.port_item}>
                     <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                        <a href="https://wize-store.vercel.app/">
-                            <img src="https://img2.pic.in.th/pic/image602158b491f5837f.png"  />
+                        <a href="https://wize-store.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <img src="https://img2.pic.in.th/pic/image602158b491f5837f.png" />
                         </a>
                     </Tilt>
                     <a href="https://wize-store.vercel.app/">
@@ -19,9 +19,11 @@ function Portfolio() {
                 </div>
                 <div className={styles.port_item}>
                     <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                        <img src="https://images.unsplash.com/photo-1619410283995-43d9134e7656?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        <a href="https://weather-app-x-simple.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <img src="https://github.com/Jiranon-K/WeatherApp/assets/132673259/a596595e-3d5c-482c-b8a7-4f92ac005606" />
+                        </a>
                     </Tilt>
-                    <p>Project Title</p>
+                    <p>WeatherApp</p>
                 </div>
                 <div className={styles.port_item}>
                     <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
