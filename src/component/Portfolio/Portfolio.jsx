8d990e59bc174636584d48b4576a479a3ce4f761
Jiranon-K <132673259+Jiranon-K@users.noreply.github.com>
2024-11-06 17:23:22 +0700

    Update Portfolio.jsx

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -41,6 +41,7 @@ function Portfolio() {
                     <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
                         <img src="https://github.com/user-attachments/assets/5ae7da1b-b0b7-4e5d-86cb-d592f82164c8" alt="" />
                     </Tilt>
+                     <a href="https://portfolio-tl-one.vercel.app/">
                     <p>Portfolio Tailwind</p>
                 </div>
                 <div className={styles.port_item}>
